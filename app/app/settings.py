55b7a0ab689e3f8e9aaa7e3ad421e8021a5cee06
LIM0310 <limsy0310@gmail.com>
2024-06-21 21:45:59 +0900

    add challegge 13

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_SYSTEM_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
 ]
+ 
+CUSTOM_USER_APPS = [
+    'users.apps.UsersConfig'
+]
+
+INSTALLED_APPS = CUSTOM_USER_APPS + DJANGO_SYSTEM_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,3 +132,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#custom user model
+AUTH_USER_MODEL = 'users.User'
