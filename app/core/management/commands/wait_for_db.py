ea00a707edd37ce07330b3b88e19f66d0fd36d10
LIM0310 <limsy0310@gmail.com>
2024-06-26 12:06:06 +0900

    add challenge

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,3 +1,7 @@
+# wait_for_db 
+# => Django가 DB가 준비될 때까지 연결을 재시도하게 해주시 위해 필요
+# => 왜 이렇게 하나요? 하나의 도커 이미지에 각 컨테이너 (app, db)가 존재하기 때문에 
+
 # Django가 DB 연결에 실패했을 시, 재시도 하도록 만드는 로직을 추가
 from django.core.management.base import BaseCommand
 from django.db import connections
--- app/core/tests.py
+++ app/core/tests.py
@@ -10,20 +10,20 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('django.db.utils.ConnectionHandler.__getitem__')
-class CommandTests(SimpleTestCase):
+@patch('django.db.utils.ConnectionHandler.__getitem__') # 이 함수를 가로챈다.
+class CommandTests(SimpleTestCase): #테스트를 위해서 테스트케이스 필요
     """Test commands."""
 
-    #db가 준비되었을때 wait_for_db가 잘 동작하는지 체크하는 함수
-    def test_wait_for_db_ready(self, patched_getitem):
+    # wait_for_db 명령어가 DB가 준비되었을 때 잘 동작하는 지 체크하는 함수
+    def test_wait_for_db_ready(self, patched_getitem): 
         """Test waiting for database if database ready."""
         patched_getitem.return_value = True
 
         call_command('wait_for_db')
 
         self.assertEqual(patched_getitem.call_count, 1)
-
-    #db연결에 오류가 발생했다고 가정을 하고 테스트
+ 
+    # db연결에 오류가 발생했다고 가정을 하고 테스트
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_getitem):
         """Test waiting for database when getting OperationalError."""
@@ -34,4 +34,4 @@ class CommandTests(SimpleTestCase):
 
         self.assertEqual(patched_getitem.call_count, 7)
 
-#docker-compose run --rm app sh -c 'python manage.py test core'
+    # docker-compose run --rm app sh -c 'python manage.py test core'
